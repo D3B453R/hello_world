9be7b86afb5352f2a0b96448cdf82ec8f27bbbc0
Maximilian Müller <mmueller@maximilians-mbp.fritz.box>
2018-05-17 20:00:47 +0200

    Initial Commit

---
+++ hello_world/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hello_world
+//
+//  Created by Maximilian Müller on 17.05.18.
+//  Copyright © 2018 Maximilian Müller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
